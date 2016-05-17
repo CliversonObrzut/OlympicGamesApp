87e6f506bf118e83fb69cc28d62a2a2a8de655c9
Cliverson <cliver_82@hotmail.com>
2016-05-17 10:44:10 +1000

    Updated the physical model and the Database spreedsheet

---
+++ OlynpicGamesApp.Tests/CustomerTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace OlynpicGamesApp.Tests
+{
+    public class CustomerTests
+    {
+        [Fact]
+        public void ContructorTest_ShouldReturnValidObject()
+        {
+            //Exercise the SUT (system under test)
+
+        }
+    }
+}
